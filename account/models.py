578ac2976bc4f3117c08dc94beee11e12fd13214
AliHHasan1 <eng.alihasan2@gmail.com>
2025-07-15 09:13:44 +0300

    create Customer model and set AUTH_USER_MODEL in settings

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Customer(AbstractUser):
+    #تم جعل الemail حقل unique
+    email = models.EmailField(unique=True, null=False, blank=False)
+    phone = models.CharField(max_length=20)
+    address = models.TextField(max_length=500)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
